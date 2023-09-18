f2bb648d7f3b139ab1b9521313762d3d288df117
Dewi Mawaddah <dewi.mawaddah@ITI-00883.jatis.com>
2023-09-19 00:17:28 +0700

    done add DDT and test case in test suites

---
+++ Test Suites/TS Login/TS Login DDT.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Login DDT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8f37b134-5ce2-46e1-88d9-4e7bddcfd827</testSuiteGuid>
+   <testCaseLink>
+      <guid>d376c3a6-4340-41f2-8a42-9b54c00f8ad3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC Manual/TC Login/TC Success Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>d51c560c-1a82-4bff-842c-301157aa1bb4</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data Login/Login Data Excel</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>d51c560c-1a82-4bff-842c-301157aa1bb4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>e0dcf33b-b91a-4228-a438-5f084837b24d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>d51c560c-1a82-4bff-842c-301157aa1bb4</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>50ac44f8-f263-44e9-8ba0-d4a8e5ddec07</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TS Register/TS Register DDT.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Register DDT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1c55e8f4-5989-4b12-86d8-e2588511daae</testSuiteGuid>
+   <testCaseLink>
+      <guid>45c701f5-05b6-47d8-b705-1908a1d0d097</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC Manual/TC Register/TC Success Register</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>2d1ec352-b6d8-4d88-bc0e-071c242db70f</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data Register/Register Data Excel</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>2d1ec352-b6d8-4d88-bc0e-071c242db70f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>e7be82b1-2b76-4f89-8984-78ba977afef9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>2d1ec352-b6d8-4d88-bc0e-071c242db70f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>591fa1cf-1125-4624-b9c6-d38eb66b6775</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
